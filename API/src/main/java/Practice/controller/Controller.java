7d23bfb95d08bf57aa92e3185bcf79c0dd3e7606
seobkeom <ajm153123@naver.com>
2023-07-14 22:06:16 +0900

    login delete

--- API/src/main/java/Practice/controller/Controller.java
+++ API/src/main/java/Practice/controller/Controller.java
@@ -1,5 +1,18 @@
 package Practice.controller;
 
-public class Controller {
+import java.io.IOException;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+	@GetMapping("api/v1/movies/{keyword}")
+	public MovieResponseDto get(@PathVariable String keyword) {
+		return 
+	}
 }
--- login/src/main/resources/static/res/js/common2.js
+++
@@ -1,9 +0,0 @@
-$(function() { 
-	$("[data-url]").click(function() { 
-		let url = $(this).attr("data-url"); 
-		location.href = url; 
-		});
-		 $(".autosubmit").change(function() { 
-			 $(this).parent("form").submit();
-			  }); 
-		  })
